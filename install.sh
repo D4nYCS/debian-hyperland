c51e17ddfbd124241219ed5bcf1986a326903f1e
D4nYCS <danielmaier2001@yahoo.de>
2023-07-14 21:21:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 username=$(id -u -n 1000)
-builddir=$(pwd)
 
 # Update packages list and update system
 apt update
@@ -17,26 +16,21 @@ apt upgrade -y
 apt install nala -y
 
 # Installing Essential Programs 
-nala install kitty thunar x11-xserver-utils unzip wget -y
+nala install kitty thunar unzip wget xorgrdp -y
 # Installing Other less important Programs
-nala install lightdm -y
+nala install lightdm xrdp kde-plasma-desktop -y
 
 # Install chrome-browser
 nala install apt-transport-https curl -y
-curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
+curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 nala update
-nala install brave-browser -y
+nala install google-chrome-stable -y
 
-# Enable graphical login and change target from CLI to GUI
+# Enable graphical login, change target from CLI to GUI and Remote Login
 systemctl enable lightdm
 systemctl set-default graphical.target
-
-# Beautiful bash
-git clone https://github.com/ChrisTitusTech/mybash
-cd mybash
-bash setup.sh
-cd $builddir
+systemctl enable xrdp
 
 # Use nala
 bash scripts/usenala
