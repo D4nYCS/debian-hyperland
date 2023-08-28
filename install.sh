30410f75a3e78cb29c37dcd19d8b33565e41dd39
D4nYCS <danielmaier2001@yahoo.de>
2023-08-28 21:42:37 +0200

    Install Hyprland

--- install.sh
+++ install.sh
@@ -16,16 +16,51 @@ apt upgrade -y
 # Install nala
 apt install nala -y
 
-# Making .config and Moving config files
-cd $builddir
-mkdir -p /home/$username/.config
-cp -R dotconfig/* /home/$username/.config/
-chown -R $username:$username /home/$username
-
 # Installing Essential Programs 
-nala install kitty thunar unzip wget -y
-# Installing Other less important Programs
-nala install lightdm xrdp kde-plasma-desktop tmux -y
+nala install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev  vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev libpango1.0-dev xdg-desktop-portal-wlr hwdata check libgtk-3-dev libsystemd-dev xwayland kitty
+
+#Create Directory, download source and excract 
+mkdir -p /home/$username/hyprland
+cd /home/$username/hyprland
+wget https://github.com/hyprwm/Hyprland/releases/download/v0.28.0/source-v0.28.0.tar.gz
+tar -xvf source-v0.28.0.tar.gz
+wget https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.31/downloads/wayland-protocols-1.32.tar.xz
+tar -xvJf wayland-protocols-1.32.tar.xz
+wget https://gitlab.freedesktop.org/wayland/wayland/-/releases/1.22.0/downloads/wayland-1.22.0.tar.xz
+tar -xzvJf wayland-1.22.0.tar.xz
+wget https://gitlab.freedesktop.org/emersion/libdisplay-info/-/releases/0.1.1/downloads/libdisplay-info-0.1.1.tar.xz
+tar -xvJf libdisplay-info-0.1.1.tar.xz
+git clone https://gitlab.freedesktop.org/emersion/libliftoff.git
+git clone https://gitlab.freedesktop.org/libinput/libinput.git
+
+cd wayland-1.22.0
+mkdir build &&
+cd    build &&
+eson setup ..            \
+      --prefix=/usr       \
+      --buildtype=release \
+      -Ddocumentation=false &&
+ninja
+ninja install
+cd ../..
+
+cd wayland-protocols-1.31
+
+mkdir build &&
+cd    build &&
+
+meson setup --prefix=/usr --buildtype=release &&
+ninja
+
+ninja install
+
+cd ../..
+
+chmod a+rw hyprland-source
+cd hyprland-source/
+
+sed -i 's/\/usr\/local/\/usr/g' config.mk
+make install
 
 # Install chrome-browser
 nala install apt-transport-https curl -y
